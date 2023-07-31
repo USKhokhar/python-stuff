e256db90b31dba08441e347633bb7c238c0ec221
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-08-01 01:55:56 +0530

    Basic Class Example

--- personal/index.py
+++ personal/index.py
@@ -1,6 +1,21 @@
-# File I/O --> Read CSV
+class Details:
+    def __init__(self, name, relation):
+        self.name = name
+        self.relation = relation
 
-with open("personal/file.csv") as file:
-    for person in file:
-        name, relation = person.rstrip().split(",")
-        print(f"{name} is the {relation}")
+def main():
+    details = getDetails()
+    print(f"{details.name} is the {details.relation}")
+
+def getDetails():
+    name = input("Enter Name: ")
+    relation = input("Enter Relation: ")
+    return Details(name, relation)
+
+if __name__ == "__main__":
+    main()
+
+# OUTPUT
+# Enter Name: Peter 
+# Enter Relation: STUD
+# Peter is the STUD
