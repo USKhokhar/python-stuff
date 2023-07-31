37c36a42dc27fd71c2812f5ef7ab74806c8eab0b
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-07-31 23:45:20 +0530

    File I/O: Read/Unsorted

--- personal/index.py
+++ personal/index.py
@@ -1,10 +1,5 @@
-# File I/O --> Write
-print("Enter 0000 to stop prompts")
-while True:
-    prompt = input("Enter Text: ")
-    
-    if prompt == "0000":
-        break
-    else:
-        with open("personal/file.txt", "a") as file:
-            file.write(f"{prompt} \n")
+# File I/O --> Read (Unsorted)
+
+with open("personal/file.txt") as names:
+    for name in names:
+        print(name.rstrip())
