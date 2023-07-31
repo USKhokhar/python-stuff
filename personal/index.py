ff0fbce8a5aa3c484c3db2d7631ad7761ba71f06
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-07-31 19:38:44 +0530

    try/accept

--- personal/index.py
+++ personal/index.py
@@ -1,20 +1,7 @@
-def binary_search(arr, target, high, low):
-    while low <= high:
-        mid = (low+high)//2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] > target:
-            high = mid -1
-        else: 
-            low = mid + 1
-    return -1
-
-listx = [1,2,3,4,5,6]
-my_target = int(input("Enter element to be found: "))
-
-flag = binary_search(listx, my_target, len(listx)-1, 0)
-
-if flag != -1:
-    print(f"{my_target} found at index {flag}")
-else:
-    print("Not found!")
+while True:
+    try:
+        prompt = int(input("Enter: "))
+        print(prompt)
+        break
+    except ValueError:
+        pass
