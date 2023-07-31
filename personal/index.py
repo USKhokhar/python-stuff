c5db6d5c0db603d2ae51343d912d5d7c956688db
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2023-08-01 02:26:36 +0530

    OOPS/Class/Custom method

--- personal/index.py
+++ personal/index.py
@@ -1,18 +1,34 @@
 class GriffinTester():
     def __init__(self, name):
-        if name not in ["peter", "louis", "meg", "chris", "brian", "stewie"]:
+        if name not in ["peter", "lois", "meg", "chris", "brian", "stewie"]:
             raise ValueError("Not A Griffin!")
         self.name = name
     def __str__(self):
         return "Griffin Spotted!"
+    def checkRel(self): 
+        match self.name:
+            case "peter":
+                return "family guy"
+            case "lois":
+                return "wife"
+            case "chris":
+                return "eldest son"
+            case "stewie":
+                return "youngest son"
+            case "brian": 
+                return "liberal son"
+            case "meg":
+                return "..."
+            case _:
+                return "Not a valid griffin!"
 
 def main():
     griffin = getGriffin()
-    print(griffin)
+    print(griffin.checkRel())
 
 def getGriffin():
     name = input("Enter Name: ")
-    return GriffinTester(name)
+    return GriffinTester(name,)
 
 if __name__ == "__main__":
     main() 
